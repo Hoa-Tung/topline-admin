cc645e7f7dbebc1f94335665dd69c94742e2b5ca
Hoa-Tung <hoa_tung@163.com>
2019-06-26 22:00:28 +0800

    用户登录-样式布局

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import router from './router'
 
+import router from './router'
+// 引入公共样式文件
+import './styles/index.less'
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,18 +1,48 @@
 <template>
-   <div class="">
-       <p>Login Compenent</p>
-   </div>
+    <div class="login-wrap">
+        <el-form class="login-from" ref="form" :model="form">
+            <el-form-item>
+                <el-input v-model="form.mobile" placeholder="请输入手机号"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-input v-model="form.code" placeholder="请输入验证码"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">登录</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 
 <script type="text/ecmascript-6">
 export default {
   name: 'AppLogin',
   data() {
-    return {}
+    return {
+      form: {
+        mobile: '',
+        code: ''
+      }
+    }
   },
-  components: {}
+  methods: {
+    onSubmit() {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
 <style scoped lang="less">
+    .login-wrap{
+        height: 100%;
+        background-color: #2b3e4a;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .login-from {
+
+        }
+    }
 </style>
